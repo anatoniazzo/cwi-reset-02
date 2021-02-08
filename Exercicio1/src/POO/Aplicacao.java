a7586c379424e262f17206d43e20dc4d453e1f76
Ana Paula <aninha.toniazzo84@gmail.com>
2021-02-08 14:47:29 -0300

    exercicios da CWI

---
+++ Exercicio1/src/POO/Aplicacao.java
@@ -0,0 +1,16 @@
+package POO;
+
+public class Aplicacao {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        //calculadora.soma(1,2);
+        System.out.println(calculadora.soma(1,2));
+        System.out.println(calculadora.subtrai(5,9));
+        System.out.println(calculadora.multiplica(10,6));
+        System.out.println(calculadora.divide(10,5));
+
+
+
+    }
+    }
+
--- projeto/src/PrimeiraClasse.java
+++ projeto/src/PrimeiraClasse.java
@@ -1,5 +1,19 @@
 public class PrimeiraClasse {
     public static void main(String[] args) {
-        System.out.println(" Aprendendo a mexer no Git");
+
+        /*int valor1 = 4;
+        int valor2 = 7;
+        int resultado = valor1 * valor2;
+        System.out.println(valor1 + "multiplicado pelo " + valor2 + " é igual  a " + resultado );*/
+
+       /* int numero1 = 6;
+        int numero2 = 3;
+        int resto = numero1 % numero2;
+        System.out.println("O resto da divisão de " + numero1 + " por " + numero2 + " é " + resto);*/
+
+        int numero1 = 10;
+        int numero2 = 9;
+
+        System.out.println(numero1 + " é maior que o " + numero2);
     }
 }
