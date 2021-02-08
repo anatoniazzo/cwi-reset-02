68303047d726a3767d1180689dd7f25f80516b5d
Ana Paula <aninha.toniazzo84@gmail.com>
2021-02-08 15:29:25 -0300

    Finalizado exercicio 3

--- Exercicio 3/src/Aplicacao.java
+++ Exercicio 3/src/Aplicacao.java
@@ -2,6 +2,12 @@ package entities;
 
 public class Aplicacao {
     public static void main(String[] args) {
+        Aluno aluno = new Aluno();
+        aluno.nome();
+        aluno.notas();
+
+
+
 
     }
 }
