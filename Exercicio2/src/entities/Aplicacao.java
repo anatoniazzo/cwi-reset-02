9e1fa125ba082afb29216ea3f640d096935569a7
Ana Paula <aninha.toniazzo84@gmail.com>
2021-02-16 15:17:00 -0300

    Ajustando pastas Aula 1

--- Exercicio2/src/entities/Aplicacao.java
+++
@@ -1,8 +0,0 @@
-package entities;
-
-public class Aplicacao {
-    public static void main(String[] args) {
-        Comparador comparador = new Comparador();
-        System.out.println(comparador.menorQue(5, 10));
-    }
-}
